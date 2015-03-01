7ca7d76c831c1f4e68df733a9d8df407998e41bf
eldar <eldar.ablaev89@yandex.ru>
2015-03-02 01:39:17 +0300

    add weapons to player

---
+++ Assets/Scripts/PlayerWeapons.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerWeapons : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+        SelectWeapon(0);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            BroadcastMessage("Fire");
+        }
+
+	    if (Input.GetKeyDown("1"))
+        {
+            SelectWeapon(0);
+        }
+        else if (Input.GetKeyDown("2"))
+        {
+            SelectWeapon(1);
+        }
+	}
+
+    private void SelectWeapon(int index)
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            if (i == index)
+            {
+                transform.GetChild(i).gameObject.SetActive(true);
+            }
+            else
+            {
+                transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RocketLauncher.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocketLauncher : MonoBehaviour {
+
+    public Rigidbody projectile;
+    public int ammoCount = 15;
+    public float reloadTime = 0.5f;
+    public float initSpeed = 20.0f;
+
+    private float lastShotTime = -10.0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    public void Fire()
+    {
+        if ( Time.time > (reloadTime + lastShotTime) && ammoCount > 0 )
+        {
+            Rigidbody projectileInstance = Instantiate(projectile, transform.position, transform.rotation) as Rigidbody;
+            projectileInstance.velocity = transform.TransformDirection(new Vector3(0, 0, initSpeed));
+            Physics.IgnoreCollision(projectileInstance.collider, transform.root.collider);
+            lastShotTime = Time.time;
+            ammoCount--;
+        }
+    }
+}
