381ca848a6ac7da3ff66d683c050be38f9fe49a4
eldar <eldar.ablaev89@yandex.ru>
2015-03-02 22:45:28 +0300

    add explosion to rocket

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -3,13 +3,41 @@ using System.Collections;
 
 public class Rocket : MonoBehaviour {
 
+    public GameObject explosion;
+    public float timeout = 3.0f;
+
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+    {
+        Invoke("KillRocket", timeout);
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 	
 	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint contact = collision.contacts[0];
+        Quaternion rotation = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        Instantiate(explosion, contact.point, rotation);
+
+        KillRocket();
+    }
+
+    private void KillRocket()
+    {
+        ParticleEmitter emitter = GetComponentInChildren<ParticleEmitter>();
+
+        if (emitter != null)
+        {
+            emitter.emit = false;
+        }
+
+        transform.DetachChildren();
+
+        Destroy(gameObject);
+    }
 }
